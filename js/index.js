c1495de599d087f344a08d35c36234f195c99643
Dafwah <joshua@fineartportrait.org>
2023-05-15 11:12:47 -0500

    Prelim Setup

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 "use strict";
+
+const create = document.getElementById("create");
+const color = document.getElementById("color");
+const mono = document.getElementById("mono");
+const clear = document.getElementById("clear");
+const mainGrid = document.querySelector(".main__grid");
+
+const gridDiv = document.createElement("div");
+
+gridDiv.id = "etchASketch";
+
+mainGrid.appendChild(gridDiv);
